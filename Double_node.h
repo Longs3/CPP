d9676b1b05626a180f065cc575c2418b0632bfa0
Longs3 <senglong0887663213@gmail.com>
2023-01-02 13:11:04 +0700

    new update

--- Double_node.h
+++ Double_node.h
@@ -1,29 +1,29 @@
-//
-// Created by LONG on 12/18/2022.
-//
-
 #ifndef DOUBLE_NODE_H
 #define DOUBLE_NODE_H
 
 template<class T>
 class Double_node {
 private:
-    Double_node<T> *next, *prev;   // Pointer to the next and previous node
+    Double_node<T>* next, * prev;   // Pointer to the next and previous node
     T data;     // Data of the current node
 public:
-    T getData() const;
-    Double_node<T>* getNext();
-    Double_node<T>* getPrev();
-    void setNext(Double_node<T>*);
-    void setPrev(Double_node<T>*);
+    Double_node(): data(NULL), next(NULL), prev(NULL) {}
+    T getData() const;      // Retrieving the data of the current node
+    void setData(T);        // Setting the data of the current node
+    Double_node<T>* getNext() const;    // Retriving the next pointer of the current node
+    Double_node<T>* getPrev() const;    // Retriving the prev pointer of the current node
+    void setNext(Double_node<T>*);      // Setting the next pointer to a specific node
+    void setPrev(Double_node<T>*);      // Setting the prev pointer to a specific node
 };
 
 template<class T>
 T Double_node<T>::getData() const { return data; }
 template<class T>
-Double_node<T>* Double_node<T>::getNext() { return next; }
+void Double_node<T>::setData(T value) { this->data = value; }
 template<class T>
-Double_node<T>* Double_node<T>::getPrev() { return prev; }
+Double_node<T>* Double_node<T>::getNext() const { return next; }
+template<class T>
+Double_node<T>* Double_node<T>::getPrev() const { return prev; }
 template<class T>
 void Double_node<T>::setNext(Double_node<T>* next) { this->next = next; }
 template<class T>
